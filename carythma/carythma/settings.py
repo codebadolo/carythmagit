f4a8118a073477425fd2a4a3e7e55bc1700aa777
codewithbadolo <codewithbadolo@gmail.com>
2023-09-22 14:13:04 +0000

    modif db

--- carythma/carythma/settings.py
+++ carythma/carythma/settings.py
@@ -71,9 +71,14 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'carythma.wsgi.application' DATABASES = { "default": { "ENGINE": "django.db.backends.sqlite3",# "NAME": 
+WSGI_APPLICATION = 'carythma.wsgi.application'
+
+DATABASES = { "default": {
+	 "ENGINE": "django.db.backends.sqlite3",# "NAME": 
         BASE_DIR / "db.sqlite3",
     }
+
+
 }
 '''
 DATABASES = {
